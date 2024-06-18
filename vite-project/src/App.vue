12aa21e999b1433b021d7e2d2f06fdd98fc99857
Tatiana Mantilla <tatanamantilla547@gmail.com>
2024-06-18 10:44:17 -0500

    Inicio del projecto

---
+++ vite-project/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="contenedor">
+  <q-layout view="hHh LpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered overlay>
+      <!-- drawer content --> 
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</div>
+
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
+
+<style>
+</style>
